de72716e523813689c74f6c9868abe9061984c32
Noah Rego Rohrbach <71419471+Noahlq@users.noreply.github.com>
2022-10-07 10:47:08 -0300

    Add files via upload

---
+++ ProjetoPokemon/ProjetoPokemon/src/main/java/poke/Conector/Conector.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poke.Conector;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author NoahRohrbach
+ */
+public class Conector {
+    
+    static String urlBD = "jdbc:mysql://localhost:3306/projetopokemon";
+    static String username = "root";
+    static String password = "1234";
+    
+    public static Connection getConnection() throws SQLException{
+        
+        Connection connection = null;
+        
+        connection = DriverManager.getConnection(urlBD, username, password);
+                
+        return connection;
+        
+    }
+    
+    
+    
+}
---
+++ ProjetoPokemon/ProjetoPokemon/src/main/java/poke/dao/PokemonDAO.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poke.dao;
+
+import poke.Conector.Conector;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import poke.Model.PokemonModel;
+
+/**
+ *
+ * @author mathe
+ */
+public class PokemonDAO {
+    
+}
